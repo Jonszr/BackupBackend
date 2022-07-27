9da8ae5bc5b862ec4c1d6785774c98539d6fa3cc
SAITIbrahim <98134542+SAITIbrahim@users.noreply.github.com>
2022-07-27 14:02:35 -0600

    Fragments
    Added additional fragments (left-side navigation)
    Added real statistics for user dashboard
    Added real project references for user

--- src/main/java/ca/sait/backup/controller/html/user/UserDashboardController.java
+++ src/main/java/ca/sait/backup/controller/html/user/UserDashboardController.java
@@ -1,12 +1,11 @@
 package ca.sait.backup.controller.html.user;
 
 
+import ca.sait.backup.mapper.ProjectMemberRepository;
 import ca.sait.backup.model.business.JWTSessionContainer;
-import ca.sait.backup.model.entity.User;
-import ca.sait.backup.model.entity.UserNotificationEnum;
-import ca.sait.backup.service.NotificationService;
-import ca.sait.backup.service.SessionService;
-import ca.sait.backup.service.UserService;
+import ca.sait.backup.model.business.RowContainer;
+import ca.sait.backup.model.entity.*;
+import ca.sait.backup.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -32,13 +31,86 @@ class UserDashboardController {
     @Autowired
     private SessionService sessionService;
 
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private SupportTicketService supportTicketService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AssetService assetService;
+
+    @Autowired
+    private ProjectMemberRepository projectMemberRepository;
+
     @GetMapping("/dashboard")
     public String GetDashboard(Model model, HttpServletRequest request) {
 
         // Expose session variables
         this.sessionService.exposeEssentialVariables(request, model);
 
- 
+        JWTSessionContainer sessionContainer = this.sessionService.extractSession(
+                request
+        );
+
+        // Get list of projects from Project Service.
+        List<Project> projectList = this.projectService.getAllProjects(
+            sessionContainer
+        );
+
+        // Sort by most categories
+        projectList.sort((Project a, Project b) -> {
+            return b.getCategories().size() - a.getCategories().size();
+        });
+
+        if (projectList.size() >= 3) {
+            projectList = projectList.subList(0, 3);
+        }
+
+        // Process project list into a grid format.
+        RowContainer<Project> gridContainer = new RowContainer<>(projectList, 3);
+
+        model.addAttribute("projectGrid", gridContainer.getGrid());
+
+        Integer numProjects = 0;
+        Integer numAssets = 0;
+        Integer numSupportTickets = 0;
+
+        User user = this.userService.dev_GetUserById(
+            sessionContainer.getUserId()
+        );
+
+
+        List<ProjectMember> membershipList = this.projectMemberRepository.findByUser(
+                user
+        );
+
+        for (Project project: user.getProjects()) {
+            for (Category cat: project.getCategories()) {
+                numAssets += cat.getAssets().size() + cat.getAssetFolders().size();
+            }
+        }
+
+        for (ProjectMember membership: membershipList) {
+            for (Category cat: membership.getProject().getCategories()) {
+                numAssets += cat.getAssets().size() + cat.getAssetFolders().size();
+            }
+        }
+
+        numSupportTickets = this.supportTicketService.getSupportTicketsForUser(
+            user
+        ).size();
+
+        numProjects = user.getProjects().size() + membershipList.size();
+
+        model.addAttribute("numAssets", numAssets);
+        model.addAttribute("numProjects", numProjects);
+        model.addAttribute("numTickets", numSupportTickets);
+        model.addAttribute("numManagers", numProjects);
+
         return ("/user/dashboard");
 
     }
